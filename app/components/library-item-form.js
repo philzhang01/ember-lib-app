911de06b17a1a0a8c6118335b9d908043d254ebf
philzhang01 <philzhang01@gmail.com>
2020-04-23 00:35:33 -0700

    create library-item and library-item form components & clearning up lib/index/edit templates with components

---
+++ app/components/library-item-form.js
@@ -0,0 +1,11 @@
+import Component from '@glimmer/component';
+import { action } from '@ember/object';
+
+export default class LibraryItemFormComponent extends Component {
+  handleClick = this.args.handleClick;
+
+  @action
+  buttonClicked(param) {
+    this.handleClick(param);
+  }
+}
